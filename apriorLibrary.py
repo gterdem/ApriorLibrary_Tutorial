12c29c98e9e345cb7504721e230b91517e16ba5f
Galip Tolga Erdem <galiperdem@gmail.com>
2020-10-20 22:22:59 +0300

    Initial commit

---
+++ apriorLibrary.py
@@ -0,0 +1,39 @@
+from typing import Counter
+import itertools
+
+
+def getIndividualSupport(dataDictionary, totalTransactions):
+    dataDict = Counter(sorted(dataDictionary))
+    supportList = []
+    for key,value in dataDict.items():
+        supportList.append((key, value/totalTransactions))
+    return supportList
+
+def getTransactions(data, printData=False):
+    transactions = []
+    for t in data.values:
+        transaction = MarketTransaction(t)
+        transactions.append(transaction)
+        if(printData):
+            print(transaction.tId, transaction.Items)
+    return transactions
+
+def getCleanData(data):
+    return  [item for sublist in data.values for item in sublist if (str(item) != 'nan' and not str(item).startswith('T'))]
+
+class MarketTransaction:
+    def __init__(self, transaction):
+        self.tId = transaction[0]
+        self.Items = []        
+        for i in range(1,len(transaction)):
+            if(str(transaction[i]) != 'nan'):
+                self.Items.append(transaction[i])
+        
+        self.Combinations = self.calculateCombinations()
+
+    def calculateCombinations(self):
+        combList = []
+        for i in range(2,len(self.Items)+1):
+            comb = itertools.combinations(self.Items, i)
+            combList.append(list(comb))
+        return combList
---
+++ tutorial-1.py
@@ -0,0 +1,42 @@
+# products = ["Apple","Beer","Rice","Meat","Milk", "Pear"]
+
+# t1 = (products[0], products[1], products[2], products[3] )
+# t2 = (products[0], products[1], products[2] )
+# t3 = (products[0], products[1] )
+# t4 = (products[0], products[5] )
+# t5 = (products[4], products[1], products[2], products[3] )
+# t6 = (products[4], products[1], products[2] )
+# t7 = (products[4], products[1] )
+# t8 = (products[4], products[5] )
+# transactions = sorted([t1,t2,t3,t4,t5,t6,t7,t8])
+# print(transactions)
+
+# for item in data.values:
+#     for i in range(0,len(item) ):
+#         if(str(item[i]) != "nan" and not str(item).startswith('T')):
+#             records.append(item[i])
+# print(records)
+# print(len(records))
+
+# apples = cleanedData.count('Apple')
+# print(apples)
+
+
+import pandas as pd
+
+import apriorLibrary as ap
+
+# Read from file
+data = pd.read_csv('marketlist.csv', header=None)
+transactions = ap.getTransactions(data)
+# print(f'Total transactions: {len(transactions)}')
+
+cleanedData = ap.getCleanData(data)
+individualSupports = ap.getIndividualSupport(cleanedData, len(transactions))
+
+for t in transactions:
+    print(f"================= {t.tId} Items ================= ")
+    print(t.Items)
+    print(f"================= {t.tId} Combinations ================= ")
+    print(t.Combinations)
+
